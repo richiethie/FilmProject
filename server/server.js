363b2ffb477e1dc521f2c6615d4d5ad66062c1f6
Richie Thiesfeldt <rthiesfeldt@roydan.com>
2024-12-16 13:26:51 -0600

    server change

--- server/server.js
+++ server/server.js
@@ -15,10 +15,19 @@ const PORT = process.env.PORT || 5000;
 // Connect to MongoDB
 connectDB();
 
+// Enable CORS with full setup
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', process.env.FRONTEND_URL || 'https://filmshare.vercel.app');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  next();
+});
+
 // Middleware
 // Enable CORS for all origins or specify frontend URL
 const corsOptions = {
-  origin: 'https://filmshare.vercel.app/', // Adjust this based on where your frontend is hosted
+  origin: process.env.FRONTEND_URL ||  'https://filmshare.vercel.app/', // Adjust this based on where your frontend is hosted
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
