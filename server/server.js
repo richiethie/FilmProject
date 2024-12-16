47e7cf6079084f6e76590eb73beb12d70be20e09
Richie Thiesfeldt <rthiesfeldt@roydan.com>
2024-12-16 15:57:01 -0600

    changes

--- server/server.js
+++ server/server.js
@@ -15,15 +15,6 @@ const PORT = process.env.PORT || 5000;
 // Connect to MongoDB
 connectDB();
 
-// Enable CORS with full setup
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', process.env.FRONTEND_URL);
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  next();
-});
-
 // Middleware
 // Enable CORS for all origins or specify frontend URL
 const corsOptions = {
